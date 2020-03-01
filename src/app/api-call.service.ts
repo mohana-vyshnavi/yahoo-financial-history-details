3283fc96806e46c917682d4611e941df7e931203
Mohana Vyshnavi <40652825+vyshnavi-tammina@users.noreply.github.com>
2020-03-01 23:28:43 +0530

    Api call chenged from static to yahoo subscription data

--- src/app/api-call.service.ts
+++ src/app/api-call.service.ts
@@ -8,6 +8,10 @@ export class ApiCallService {
   }
   getData() {
     // http get call for getting the data from json file in assets
-    return this.http.get('./assets/data.json');
+    return this.http.get('https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data?frequency=1d&filter=history&period1=1546448400&period2=1562086800&symbol=AMRN', 
+    { headers : 
+           "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+           "x-rapidapi-key": "12c969ef39msh2f1457a5fad281dp1824c0jsn6327fc38b570"
+    });
   }
 }
